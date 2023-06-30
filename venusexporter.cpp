83fddff49848f04a6edca1e29b9e90ab5f7688da
SELEKCJONER <136343482+SELEKCJONER@users.noreply.github.com>
2023-06-30 03:44:21 +0200

    Fix swipes being added as normal responses
    and include mentions of chub api and chub itself in program as i forgot to do it earlier

--- venusexporter.cpp
+++ venusexporter.cpp
@@ -66,7 +66,7 @@ std::string current_date_time() {
 
 int main() {
     int idczatu;
-    std::cout << "IMPORTANT NOTES!!! Your chat MUST be public otherwise exporter wont be able to access it through official venus.chub.ai api endpoint! https://api.characterhub.org/api/venus/chats/ \n\n api documentation can be accessed from here https://github.com/CharHubAI/CharHub-API \n\nChat id is that number at the end of link that appears in search bar when you are in chat on venus\n\nNow please input the chat id\n\n";
+    std::cout << "IMPORTANT NOTES!!! Your chat MUST be public otherwise exporter wont be able to access it through official www.chub.ai/ api endpoint! https://api.characterhub.org/api/venus/chats/ \n\n api documentation can be accessed from here https://github.com/CharHubAI/CharHub-API \n\nChat id is that number at the end of link that appears in search bar when you are in chat on venus\n\nNow please input the chat id\n\n";
     std::cin >> idczatu;
 
     HINTERNET hInternet, hConnect;
